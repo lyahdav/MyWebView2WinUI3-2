3c89996a9605adee3d941bc7df127cc6a4766c44
Liron Yahdav <lyahdav@users.noreply.github.com>
2022-11-14 13:08:13 -0800

    Add project files.

---
+++ MyWebView2WinUI3-2/MainWindow.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::MyWebView2WinUI3_2::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::MyWebView2WinUI3_2::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
